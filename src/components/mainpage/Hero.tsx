9b0714c53a4de0a219e07605ed960b84c4c1d7b5
0xDas <126429597+Callmedas69@users.noreply.github.com>
2024-11-01 11:09:34 +0700

    Update Hero.tsx

--- src/components/mainpage/Hero.tsx
+++ src/components/mainpage/Hero.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 import localFont from "next/font/local";
 import Image from "next/image";
 import background from "@/assets/homepage/TopBG.png";
-import hero from "@/assets/homepage/Hero.png";
+import hero from "@/assets/homepage/Hero2.svg";
 import launch from "@/assets/homepage/HeroFIGHTLaunch.png";
 
 export const BackWild = localFont({
@@ -26,39 +26,53 @@ const Hero = () => {
   };
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
+    <div className="relative w-screen h-screen overflow-hidden flex justify-center items-center">
       {/* Background Image */}
-      <div className="absolute saturate-200 inset-0 -z-10">
+      {/* <div className="absolute inset-0 -z-10">
         <Image
           src={background}
           alt="background"
-          objectFit="fill"
-          objectPosition="center"
-          className="w-full h-full"
+          className="w-full h-full object-cover"
+          priority
+          quality={100}
+          fill
         />
-      </div>
+      </div> */}
+      <div
+        className="absolute inset-0 bg-cover bg-center contrast-200 brightness-50 saturate-200"
+        style={{ backgroundImage: `url(${background.src})` }}
+      ></div>
 
-      {/* Launch and Hero Section */}
-      <div className="relative flex flex-row w-full h-screen justify-center items-center bottom-0 z-10">
-        {/* Launch Image */}
-        <div className="z-40 justify-end">
-          <div>
-            <Image src={launch} alt="launch" />
-          </div>
-          <div className="flex justify-center items-center">
+      {/* Content Container */}
+      <div className="relative flex flex-col lg:flex-row items-center justify-center lg:justify-between max-w-[1100px] w-full h-full px-4 lg:px-0 lg:space-x-10 space-y-6 lg:space-y-0">
+        {/* Launch Image and Button */}
+        <div className="pl-[10%]">
+          <div className="flex flex-col items-center z-40 space-y-4">
+            <Image
+              src={launch}
+              alt="launch"
+              width={250} // Reduced width by 50%
+              height={250} // Reduced height by 50%
+              className="w-[20vw] max-w-[200px] lg:max-w-[250px] h-auto" // Updated width classes for scaling
+            />
             <Button
               onClick={scrollToSection}
-              className="bg-yellow-500 rounded-none text-2xl text-black font-semibold px-10 py-7 hover:bg-white  hover:shadow-2xl hover:shadow-orange-300"
+              className="justify-center items-center mx-auto my-auto bg-yellow-500 rounded-none text-lg md:text-2xl text-black font-semibold px-6 md:px-10 py-4 md:py-7 hover:bg-white hover:shadow-2xl hover:shadow-orange-300"
             >
               HOW TO BUY $TTS
             </Button>
           </div>
         </div>
 
-        {/* Hero Image at the Bottom */}
-
-        <div className="h-full content-end pl-48 pb-20 transform scale-150 aspect-square z-10">
-          <Image src={hero} alt="hero" />
+        {/* Hero Image (Proportionally Scaled and Positioned at Bottom) */}
+        <div className="lg:flex hidden absolute bottom-0 right-0 z-10">
+          <Image
+            src={hero}
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="w-[50vw] max-w-[600px] h-auto"
+          />
         </div>
       </div>
     </div>
